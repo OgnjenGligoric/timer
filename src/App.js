6f84ec29266bee62bcd5e4051b129b463ef86ae2
OgnjenGligoric <gligoric383@gmail.com>
2024-09-23 14:32:04 +0200

    react router Link and exact path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Component} from "react";
+import {Link} from 'react-router-dom'
+
+class Home extends Component {
+    render() {
+        return null;
+    }
+}
 
 function App() {
   return (
       <Router>
         <div className="App">
           <Switch>
-            <Route path = "/">
-              {/*    home*/}
+            <Route exact path = "/">
+              <Home />
             </Route>
           </Switch>
         </div>
