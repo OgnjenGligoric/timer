b502ef93ca8724a84d5be82ecc602473bb59b28a
OgnjenGligoric <gligoric383@gmail.com>
2024-09-26 23:02:06 +0200

    feat: indefinite linear animation

--- src/components/TimerProgressbar.tsx
+++ src/components/TimerProgressbar.tsx
@@ -1,16 +1,16 @@
 import { buildStyles, CircularProgressbarWithChildren } from "react-circular-progressbar";
 import React from "react";
+import './TimerProgressbar.css'
 
 const TimerProgressbar: React.FC<TimerProgressbarProps> = ({ title, endTime, timeLeft }) => {
     const percentage = 100 - (timeLeft / endTime) * 100;
-    const pathColor = timeLeft === 0 ? '#cb6767' : '#67cb88';
 
     return (
         <CircularProgressbarWithChildren
             value={percentage}
             strokeWidth={5}
             styles={buildStyles({
-                pathColor,
+                pathColor: timeLeft === 0 ? 'url(#color-shift)' : '#67cb88',  // Use animated gradient when timeLeft is 0
                 textColor: '#ffffff',
                 trailColor: '#545576',
             })}
@@ -20,6 +20,31 @@ const TimerProgressbar: React.FC<TimerProgressbarProps> = ({ title, endTime, tim
                 <h1>{formatTime(endTime - timeLeft)}</h1>
                 <p>{formatTime(timeLeft)} left</p>
             </div>
+
+            {timeLeft === 0 && (
+                <svg id="color-shift-svg">
+                    <defs>
+                        <linearGradient id="color-shift" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <stop offset="0%" stopColor="#67cb88">
+                                <animate
+                                    attributeName="stop-color"
+                                    values="#67cb88;#cb6767;#67cb88"
+                                    dur="2s"
+                                    repeatCount="indefinite"
+                                />
+                            </stop>
+                            <stop offset="100%" stopColor="#cb6767">
+                                <animate
+                                    attributeName="stop-color"
+                                    values="#cb6767;#67cb88;#cb6767"
+                                    dur="2s"
+                                    repeatCount="indefinite"
+                                />
+                            </stop>
+                        </linearGradient>
+                    </defs>
+                </svg>
+            )}
         </CircularProgressbarWithChildren>
     );
 };
@@ -32,9 +57,8 @@ type TimerProgressbarProps = {
     timeLeft: number;
 }
 
-const formatTime = (time: number) : string => {
+const formatTime = (time: number): string => {
     const minutes = String(Math.floor(time / 60)).padStart(2, '0');
     const seconds = String(time % 60).padStart(2, '0');
-    return  minutes + ':' + seconds;
+    return minutes + ':' + seconds;
 };
-
