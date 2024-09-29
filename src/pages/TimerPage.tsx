a814356e2221c91001f6e3f20a327669ca80b299
OgnjenGligoric <gligoric383@gmail.com>
2024-09-29 15:27:26 +0200

    feat: Error boundaries for timer

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,5 +1,12 @@
 import TimerComponent from "../components/timer/Timer";
-import './Timerpage.css'
-export default function TimerPage(){
-    return <TimerComponent title={"Title from Props"} endTime={4} elapsedTime={2}></TimerComponent>;
-};
+import './Timerpage.css';
+import {ErrorBoundary, FallbackProps} from "react-error-boundary";
+import ErrorFallback from "../utils/ErrorFallback";
+
+export default function TimerPage() {
+    return (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <TimerComponent title={"Title from Props"} endTime={4} elapsedTime={6} />
+        </ErrorBoundary>
+    );
+}
