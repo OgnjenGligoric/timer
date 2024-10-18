0ff690eb028c6fa9872157ec29e8910355ba3890
OgnjenGligoric <gligoric383@gmail.com>
2024-10-18 12:54:24 +0200

    feat: stories for timer form and timer page

---
+++ src/stories/TimerForm.stories.tsx
@@ -0,0 +1,15 @@
+import { fn } from "@storybook/test";
+import TimerForm from "../components/timer-form/TimerForm";
+
+export default {
+    component: TimerForm,
+    title: "TimerForm",
+    tags: ['autodocs'],
+    args: {
+    },
+};
+
+export const Default = {
+    args: {
+    }
+};
